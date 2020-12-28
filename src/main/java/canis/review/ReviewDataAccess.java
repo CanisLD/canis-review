c1f1145b303855a5b94eacb9a1346df2d87f04db
Smith Surasmith <ssurasmith+dev@gmail.com>
2020-12-28 15:41:15 -0800

    Moved data logic to ReviewDataAccess. Added ReviewDataAccess.findById(String)

--- src/main/java/canis/review/ReviewDataAccess.java
+++ src/main/java/canis/review/ReviewDataAccess.java
@@ -1,6 +1,9 @@
 package canis.review;
 
+import java.time.Instant;
 import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,8 +18,25 @@ public class ReviewDataAccess {
     return reviewRepository.findBySubject(subject);
   }
 
-  public void save(Review review) {
+  public Optional<Review> findById(String review) {
+    return reviewRepository.findById(review);
+  }
+
+  public String save(Review candidate) {
+    final Review review = new Review.ReviewBuilder()
+      .instant(Instant.now())
+      .review(UUID.randomUUID().toString())
+      .author(candidate.getAuthor())
+      .subject(candidate.getSubject())
+      .header(candidate.getHeader())
+      .byline(candidate.getByline())
+      .body(candidate.getBody())
+      .link(candidate.getLink())
+      .score(candidate.getScore())
+      .tag(candidate.getTag())
+      .build();
     reviewRepository.save(review);
+    return review.getReview();
   }
   
 }
--- src/main/java/canis/review/ReviewServiceController.java
+++ src/main/java/canis/review/ReviewServiceController.java
@@ -1,9 +1,7 @@
 package canis.review;
 
-import java.time.Instant;
 import java.util.Collection;
-// import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,36 +19,20 @@ public class ReviewServiceController {
   @Autowired
   private ReviewDataAccess reviewDataAccess;
 
-  @GetMapping("/{subject}")
-  public Collection<Review> getReview(
-    @PathVariable(value = "subject") String subject) {
+  @GetMapping("/subject/{subject}")
+  public Collection<Review> getReviewBySubject(@PathVariable(value = "subject") String subject) {
     return reviewDataAccess.findBySubject(subject);
-    // return List.of(new Review.ReviewBuilder()
-    //   .instant(Instant.now())
-    //   .author("Smith")
-    //   .subject(subject)
-    //   .score(0.7f)
-    //   .header("header")
-    //   .body("Ipsum Lorem")
-    //   .tag(List.of("English","Test"))
-    //   .build());    
+  }
+
+  @GetMapping("/{review}")
+  public Review getReviewById(@PathVariable(value = "review") String review) {
+    final Optional<Review> reviewOptional = reviewDataAccess.findById(review);
+
+    return reviewOptional.orElse(null);
   }
 
   @PostMapping("/add")
   public String writeReview(@RequestBody Review candidate) {
-    final Review review = new Review.ReviewBuilder()
-      .instant(Instant.now())
-      .review(UUID.randomUUID().toString())
-      .author(candidate.getAuthor())
-      .subject(candidate.getSubject())
-      .header(candidate.getHeader())
-      .byline(candidate.getByline())
-      .body(candidate.getBody())
-      .link(candidate.getLink())
-      .score(candidate.getScore())
-      .tag(candidate.getTag())
-      .build();
-    reviewDataAccess.save(review);
-    return review.getReview();
+    return reviewDataAccess.save(candidate);
   }
 }
